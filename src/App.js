6e37de2af539d8238f576f1a530e41eb44abe7a5
mitsunori.morita <mitsunori.morita@speee.jp>
2018-10-26 19:01:39 +0900

    Settings localStrage

--- src/App.js
+++ src/App.js
@@ -26,13 +26,50 @@ const Bar = styled.div`
 
 const Content = styled.div``;
 
+const checkFirstVisit = () => {
+  let cryptoDashData = localStorage.getItem("cryptoDash");
+  if (!cryptoDashData) {
+    return {
+      firstVisit: true,
+      page: "settings"
+    };
+  }
+};
+
 class App extends Component {
   state = {
-    page: "dashboard"
+    page: "dashboard",
+    ...checkFirstVisit()
   };
 
   displayingDashboard = () => this.state.page === "dashboard";
   displayingSettings = () => this.state.page === "settings";
+  firstVisitMessage = () => {
+    if (this.state.firstVisit) {
+      return (
+        <div>
+          Welcome to CryptoDash, please select your favorite coins to begin.
+        </div>
+      );
+    }
+  };
+
+  confirmFavorites = () => {
+    localStorage.setItem("cryptoDash", "test");
+    this.setState({
+      firstVisit: false,
+      page: "dashboard"
+    });
+  };
+
+  settingsContent = () => {
+    return (
+      <div>
+        {this.firstVisitMessage()}
+        <div onClick={this.confirmFavorites}>Confirm Favorites</div>
+      </div>
+    );
+  };
 
   render() {
     return (
@@ -40,14 +77,16 @@ class App extends Component {
         <Bar>
           <Logo>CryptoDash</Logo>
           <div />
-          <ControlButton
-            onClick={() => {
-              this.setState({ page: "dashboard" });
-            }}
-            active={this.displayingDashboard()}
-          >
-            Dashboard
-          </ControlButton>
+          {!this.state.firstVisit && (
+            <ControlButton
+              onClick={() => {
+                this.setState({ page: "dashboard" });
+              }}
+              active={this.displayingDashboard()}
+            >
+              Dashboard
+            </ControlButton>
+          )}
           <ControlButton
             onClick={() => {
               this.setState({ page: "settings" });
@@ -57,10 +96,7 @@ class App extends Component {
             Setting
           </ControlButton>
         </Bar>
-        <Content>
-          Hello I'm content,
-          {this.state.page}
-        </Content>
+        <Content>{this.displayingSettings() && this.settingsContent()}</Content>
       </AppLayout>
     );
   }
